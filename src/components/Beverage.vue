aaad0e242ecc277257e23a798fa8206ffe3b7b32
Bartosz Rackowski <brac193@gmail.com>
2021-11-07 11:57:24 +0100

    add beverage component

---
+++ src/components/Beverage.vue
@@ -0,0 +1,17 @@
+<template>
+  <button @click="count++">Ilość {{ kind }} - {{ count }}</button>
+</template>
+
+<script>
+export default {
+  name: "Beverage",
+  props: {
+    kind: String,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+};
+</script>
